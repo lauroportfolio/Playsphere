138b00ff6386bcac0437b0366bd690dd699e7866
Lauro <lauroportfolio@gmail.com>
2025-10-31 21:27:50 -0300

    Décimo Quinto Commit

--- next.config.ts
+++ next.config.ts
@@ -7,27 +7,14 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {},
   },
-
   serverExternalPackages: ["mongoose"],
-
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.clerk.com"
-      },
-      {
-        protocol: "https",
-        hostname: "images.clerk.dev"
-      },
-      {
-        protocol: "https",
-        hostname: "uploadthing.com"
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co"
-      },
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "images.clerk.dev" },
+      { protocol: "https", hostname: "uploadthing.com" },
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "5q51wo4dho.ufs.sh" }, // <--- adicionar o domínio que você recebeu do Clerk
     ],
   },
 };
